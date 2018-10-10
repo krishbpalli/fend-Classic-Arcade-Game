a1cca6657f8afa0129ed795f54bfd1bc5bb26f02
Krishna Boddepalli <kboddepalli@gmail.com>
2018-10-10 01:48:45 -0400

    adding styles

--- js/app.js
+++ js/app.js
@@ -34,6 +34,29 @@ let Player=function(x,y){
   this.lives=5;
 };
 
+Player.prototype.update = function() {
+    if (this.x < 0 || this.x > 400) {
+        if (this.x < 0) {
+            this.x = 0;
+        } else {
+            this.x = 400;
+        }
+    }
+    if (this.y < 0 || this.y > 400) {
+        if (this.y < 0) {
+            this.score++;
+            document.getElementsByClassName('score')[0].innerHTML = this.score;
+            if (this.score >=3 ){
+                Winner();
+            }
+            this.reset();
+        } else {
+            this.y = 400;
+        }
+    }
+    this.checkCollisions();
+};
+
 Player.prototype.render = function(x, y) {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
@@ -49,6 +72,77 @@ Player.prototype.handleInput = function(direction) {
         this.x = this.x + 101;
     }
 };
+
+Player.prototype.checkCollisions = function() {
+    for (let i = 0, len = allEnemies.length; i < len; i++) {
+        /**  e.g. var i = 0, len = <object/array>.length */
+        /** Statements */
+        if (this.x < allEnemies[i].x + allEnemies[i].width &&
+            this.x + this.width > allEnemies[i].x &&
+            this.y < allEnemies[i].y + allEnemies[i].height &&
+            this.height + this.y > allEnemies[i].y) {
+              this.lives--;
+              document.getElementsByClassName('lives')[0].innerHTML = this.lives;
+            if (this.lives === 0){
+                LostGame();
+            }
+            this.reset();
+          }
+        }
+          };
+
+  Winner=function(){
+    let body= document.getElementsByClassName('body')[0];
+      body.innerHTML = "";
+      let para = document.createElement("h6");               // Create a <p> element
+      let text = document.createTextNode("You Won");      // Create a text node
+      para.appendChild(text);                                // Append the text to <p>
+      body.appendChild(para);                                // Append <p> to body
+      var but = document.createElement("button");
+      but.value="replay";
+      let replay = document.createTextNode("Play Again");
+      but.appendChild(replay);
+      but.setAttribute("onclick","reload()");
+      but.onclick = reload;
+      body.appendChild(but);
+  }
+
+  reload=function(){
+    location.reload();
+  }
+
+LostGame=function(){
+
+  let body= document.getElementsByClassName('body')[0];
+    body.innerHTML = "";
+    let para = document.createElement("h6");               // Create a <p> element
+    let text = document.createTextNode('You Lost');
+    para.appendChild(text);                                // Append the text to <p>
+    body.appendChild(para);                                // Append <p> to body
+    var but = document.createElement("button");
+    but.value="replay";
+    let replay = document.createTextNode("Play Again");
+    but.appendChild(replay);
+    but.setAttribute("onclick","reload()");
+    but.onclick = reload;
+    body.appendChild(but);
+}
+
+Player.prototype.reset=function(){
+  this.x=200;
+  this.y=450;
+}
+
+var allEnemies = [
+    new Enemy(-200, 65, 50),
+    new Enemy(-200, 65, 85),
+    new Enemy(-200, 142, 60),
+    new Enemy(-200, 142, 95),
+    new Enemy(-200, 225, 75),
+    new Enemy(-200, 225, 55)
+];
+
+var player = new Player(200, 450);
 
 // Now write your own player class
 // This class requires an update(), render() and
