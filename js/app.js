ea07179a2853e54d21493beb816afbb25ebf355b
Krishna Boddepalli <kboddepalli@gmail.com>
2018-10-10 01:21:08 -0400

    adding player class

--- js/app.js
+++ js/app.js
@@ -1,21 +1,53 @@
 // Enemies our player must avoid
-var Enemy = function(x,y,speed) {
+let Enemy = function(x,y,speed) {
   this.x=x;
   this.y=y;
   this.speed=spped;
   this.height=40;
   this.width=60;
   this.sprite = 'images/enemy-bug.png';
+};
 
 Enemy.prototype.update = function(dt) {
+  this.x=this.x+this.speed*dt;
+
+  if (this.x > 500) {
+        this.x = 0;
+    }
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
 };
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+Enemy.prototype.render = function(x,y) {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+let Player=function(x,y){
+  this.x=x;
+  this.y=y;
+  this.sprite='images/char-pink-girl.png';
+  this.height=40;
+  this.width=70;
+  this.score=0;
+  this.lives=5;
+};
+
+Player.prototype.render = function(x, y) {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(direction) {
+    if (direction === "up") {
+        this.y = this.y - 83;
+    } else if (direction === "down") {
+        this.y = this.y + 83;
+    } else if (direction === "left") {
+        this.x = this.x - 101;
+    } else if (direction === "right") {
+        this.x = this.x + 101;
+    }
 };
 
 // Now write your own player class
